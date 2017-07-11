d97af707dcf2f5a60ac7834142b86b3a0adcfeb6
Armard Bellamy <Armard@Armards-MacBook.local>
2017-07-10 21:54:45 -0400

    Initial commit

---
+++ src/components/Coursesales.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Coursesales extends Component {
+  render(){
+    return(
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default Coursesales;
